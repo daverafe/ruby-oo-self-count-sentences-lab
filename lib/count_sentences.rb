12619bee384868ef91a369a10fd4a4e0903c65ea
David Rafe <david.rafe@gmail.com>
2020-11-27 20:55:25 -0500

    commit tree 0f34e0bbbb55fb68ae3f112b40d42a7a12245947

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(".").count 
+    sentences 
+    complex_sentences = self.gsub("!", ".").split(".").count 
+    complex_sentences 
   end
 end
